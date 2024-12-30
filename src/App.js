e45a2bc208ddc3e5c24f9093ce6d2094580790d5
Rob Bundy <robbielbundy@Gmail.com>
2024-12-30 02:18:27 -0500

    Contact Page and Footer Work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Home';
-import Projects from './Projects';
 import Section1 from './Section1';
 import Section2 from './Section2';
 import Section3 from './Section3';
 import SkillsPage from "./SkillsPage";
+import Footer from './Footer';
+import ContactMePage from "./ContactMe";
+
 const App = () => {
     return (
         <Router>
@@ -15,7 +17,9 @@ const App = () => {
                 <Route path="/Website/section2" element={<Section2 />} />
                 <Route path="/Website/section3" element={<Section3 />} />
                 <Route path="/Website/skills" element={<SkillsPage />} />
+                <Route path="/Website/contactme" element={<ContactMePage />} />
             </Routes>
+            <Footer/>
         </Router>
     );
 };
