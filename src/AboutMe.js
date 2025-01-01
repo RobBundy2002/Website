000a592a755d0dfc4a45b67522a77bae3c21d8e8
Rob Bundy <robbielbundy@Gmail.com>
2025-01-01 01:51:30 -0500

    Stylistic Changes

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,10 +7,18 @@ const AboutMePage = () => {
         <div className="about-me-container">
             <h1>About Me</h1>
             <p>
-                Rob Bundy is a software developer and current 4th year student and teaching assistant at the University of Virginia. Rob has developed and implemented with a wide range of languages and technologies, including Java, Python, React, SQLite, Bash, and Docker.
+                Rob Bundy is a software developer and current 4th year student and teaching assistant at the University
+                of Virginia. Rob grew up in a small town in Southwest Virginia, but has lived in Charlottesville mostly
+                full time since August 2022.
             </p>
             <p>
-                Outside of his course work coding, Rob enjoys exploring the world of game development, working on his mobile app, or working on website maintenance for his other projects. When he's not writing code, you'll find him playing disc golf, listening to Forrest Frank, or hooping at the local UVA gym..
+                Rob is currently applying for graduate school to work towards his Masters in Computer Science, as well
+                as planning on working full time as a software engineer starting in the summer.
+            </p>
+            <p>
+                Some of Rob's main coding interests include exploring the world of game development, working on his
+                mobile app, or website maintenance and advancement for his other projects. When he's not writing code, you'll
+                find him playing disc golf, listening to Forrest Frank, or hooping at the local UVA gym.
             </p>
             <Link to="/Website/" className="back-to-home3">
                 Back to Home
