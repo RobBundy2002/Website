399bf67efe64fcba0d1ebb65a14451d62089f4aa
Rob Bundy <robbielbundy@Gmail.com>
2025-02-18 22:55:01 -0500

    Added the Vampire Frostbite Page

--- src/ClassAssignments/ClassAssignments.js
+++ src/ClassAssignments/ClassAssignments.js
@@ -18,6 +18,13 @@ const ClassAssignments = () => {
             description: "CRUD App built with JavaFx and Sqlite which serves as an interface to add, delete, and edit CR's, and add users and classes to the database. Built at the University of Virginia for CS3140 alongside Matt Crowe and Patrick Williamson",
             imageUrl: "Images/Course Review App.png",
             link: "/Website/coursereviewapp"
+        },
+        {
+            id: 3,
+            title: "Vampire Frost Bite",
+            description: "Pico-8 game coded with Lua, that also includes various designed sprites and sounds to enhance the player experience whilst also fitting the game jam theme assigned of single room. Built at the University of Virginia for CS4730",
+            imageUrl: "Images/VampireFrostbite.png",
+            link: "/Website/vampirefrostbite"
         }
     ];
 
@@ -31,7 +38,7 @@ const ClassAssignments = () => {
                             src={project.imageUrl}
                             alt={project.title}
                         />
-                        {project.title === "Skyward Bound" || project.title === "Course Review App"? (
+                        {project.title === "Vampire Frost Bite" ? (
                             <Link to={project.link}>
                                 <h3>{project.title}</h3>
                             </Link>
@@ -40,6 +47,7 @@ const ClassAssignments = () => {
                         )}
                         <p>{project.description}</p>
                     </div>
+
                 ))}
             </div>
 
